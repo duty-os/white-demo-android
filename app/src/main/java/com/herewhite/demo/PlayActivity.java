6d7c257fde034e87a3e9c20cc8e605270798b183
buhe <buhe.bh@antfin.com>
2019-03-09 14:07:40 +0800

    upgrade to 2.0.0-beta4

--- app/src/main/java/com/herewhite/demo/PlayActivity.java
+++ app/src/main/java/com/herewhite/demo/PlayActivity.java
@@ -3,39 +3,22 @@ package com.herewhite.demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.herewhite.sdk.AbstractRoomCallbacks;
+import com.herewhite.sdk.AbstractPlayerEventListener;
 import com.herewhite.sdk.Logger;
 import com.herewhite.sdk.Player;
-import com.herewhite.sdk.PlayerEventListener;
-import com.herewhite.sdk.Room;
-import com.herewhite.sdk.RoomParams;
 import com.herewhite.sdk.WhiteBroadView;
 import com.herewhite.sdk.WhiteSdk;
 import com.herewhite.sdk.WhiteSdkConfiguration;
 import com.herewhite.sdk.domain.DeviceType;
-import com.herewhite.sdk.domain.EventEntry;
-import com.herewhite.sdk.domain.EventListener;
 import com.herewhite.sdk.domain.PlayerConfiguration;
 import com.herewhite.sdk.domain.PlayerPhase;
 import com.herewhite.sdk.domain.PlayerState;
 import com.herewhite.sdk.domain.Promise;
-import com.herewhite.sdk.domain.RoomPhase;
-import com.herewhite.sdk.domain.RoomState;
 import com.herewhite.sdk.domain.SDKError;
 import com.herewhite.sdk.domain.UpdateCursor;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
-
-import static com.herewhite.demo.MainActivity.EVENT_NAME;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -58,52 +41,52 @@ public class PlayActivity extends AppCompatActivity {
                 new WhiteSdkConfiguration(DeviceType.touch, 10, 0.1));
         PlayerConfiguration playerConfiguration = new PlayerConfiguration();
         playerConfiguration.setRoom(uuid);
-        playerConfiguration.setAudioUrl("https://ohuuyffq2.qnssl.com/98398e2c5a43d74321214984294c157e_60def9bac25e4a378235f6249cae63c1.m3u8");
+        playerConfiguration.setAudioUrl(m3u8);
 
-        whiteSdk.createPlayer(playerConfiguration, new PlayerEventListener() {
+        whiteSdk.createPlayer(playerConfiguration, new AbstractPlayerEventListener() {
             @Override
-            public void onPhaseChanged(PlayerPhase playerPhase) {
-
+            public void onPhaseChanged(PlayerPhase phase) {
+                showToast(gson.toJson(phase));
             }
 
             @Override
             public void onLoadFirstFrame() {
-
+                showToast("onLoadFirstFrame");
             }
 
             @Override
-            public void onSliceChanged(String s) {
-
+            public void onSliceChanged(String slice) {
+                showToast(slice);
             }
 
             @Override
-            public void onPlayerStateChanged(PlayerState playerState) {
-
+            public void onPlayerStateChanged(PlayerState modifyState) {
+                showToast(gson.toJson(modifyState));
             }
 
             @Override
-            public void onStoppedWithError(SDKError sdkError) {
-
+            public void onStoppedWithError(SDKError error) {
+                showToast(error.getJsStack());
             }
 
             @Override
-            public void onScheduleTimeChanged(long l) {
-
+            public void onScheduleTimeChanged(long time) {
+                showToast(time);
             }
 
             @Override
-            public void onCatchErrorWhenAppendFrame(SDKError sdkError) {
-
+            public void onCatchErrorWhenAppendFrame(SDKError error) {
+                showToast(error.getJsStack());
             }
 
             @Override
-            public void onCatchErrorWhenRender(SDKError sdkError) {
-
+            public void onCatchErrorWhenRender(SDKError error) {
+                showToast(error.getJsStack());
             }
 
             @Override
             public void onCursorViewsUpdate(UpdateCursor updateCursor) {
-
+                showToast(gson.toJson(updateCursor));
             }
         }, new Promise<Player>() {
             @Override
