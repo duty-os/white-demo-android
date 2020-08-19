e36eba5f7c57b23495fc376269d93aaba5a316aa
az <beingszhang@gmail.com>
2020-08-19 17:29:49 +0800

    feat(dynamic-ppt): support audio mixing

--- app/src/main/java/com/herewhite/demo/RoomActivity.java
+++ app/src/main/java/com/herewhite/demo/RoomActivity.java
@@ -1,18 +1,26 @@
 package com.herewhite.demo;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.alibaba.sdk.android.httpdns.HttpDns;
@@ -60,12 +68,37 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+import io.agora.rtc.video.VideoEncoderConfiguration;
 import wendu.dsbridge.DWebView;
 
 public class RoomActivity extends AppCompatActivity {
 
     /** 和 iOS 名字一致 */
     final String EVENT_NAME = "WhiteCommandCustomEvent";
+
+    // rtc 客户端
+    private RtcEngine mRtcEngine;
+    private static final int PERMISSION_REQ_ID = 22;
+
+    // 如果需要保存 rtc 日志到 sdk 卡就需要 WRITE_EXTERNAL_STORAGE 权限
+    private static final String[] REQUESTED_PERMISSIONS = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    // rtc 远端视频窗口
+    private FrameLayout mLocalContainer;
+    // rtc 本地视频窗口
+    private FrameLayout mRemoteContainer;
+    private SurfaceView mLocalView;
+    private SurfaceView mRemoteView;
+    private boolean mCallEnd = true;
+    private ImageView mCallBtn;
 
     final String SCENE_DIR = "/dir";
     final String ROOM_INFO = "room info";
@@ -76,8 +109,67 @@ public class RoomActivity extends AppCompatActivity {
     private String uuid;
     private String roomToken;
 
+    private WhiteSdk whiteSdk;
+
     WhiteboardView whiteboardView;
     Room room;
+
+    // rtc 回调
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // 注册 onJoinChannelSuccess 回调。
+        // 本地用户成功加入频道时，会触发该回调。
+        public void onJoinChannelSuccess(String channel, final int uid, int elapsed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("agora","Join channel success, uid: " + (uid & 0xFFFFFFFFL));
+                }
+            });
+        }
+
+        @Override
+        // 注册 onFirstRemoteVideoDecoded 回调。
+        // SDK 接收到第一帧远端视频并成功解码时，会触发该回调。
+        // 可以在该回调中调用 setupRemoteVideo 方法设置远端视图。
+        public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("agora","First remote video decoded, uid: " + (uid & 0xFFFFFFFFL));
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+
+        private void setupRemoteVideo(int uid) {
+            mRemoteView = RtcEngine.CreateRendererView(getBaseContext());
+            mRemoteContainer.addView(mRemoteView);
+            // 设置远端视图。
+            mRtcEngine.setupRemoteVideo(new VideoCanvas(mRemoteView, VideoCanvas.RENDER_MODE_HIDDEN, uid));
+        }
+
+
+        @Override
+        // 注册 onUserOffline 回调。
+        // 远端用户离开频道或掉线时，会触发该回调。
+        public void onUserOffline(final int uid, int reason) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("agora","User offline, uid: " + (uid & 0xFFFFFFFFL));
+                }
+            });
+        }
+
+        @Override
+        // 混音状态变化时的回调
+        public void onAudioMixingStateChanged(int state, int errorCode) {
+            if (whiteSdk != null) {
+                whiteSdk.getAudioMixerImplement().setMediaState(state, errorCode);
+            }
+        }
+    };
 
     /**
      * 自定义 GlobalState 示例
@@ -96,16 +188,21 @@ public class RoomActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        whiteboardView.removeAllViews();
-        whiteboardView.destroy();
-    }
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+
+        // 如果用户需要用到 rtc 混音功能来解决回声和声音抑制问题，那么必须要在 whiteSDK 之前初始化 rtcEngine
+        if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[2], PERMISSION_REQ_ID)) {
+            initializeEngine();
+            setupVideoConfig();
+        }
+        mCallBtn = findViewById(R.id.btn_call);
+        mLocalContainer = findViewById(R.id.local_video_view_container);
+        mRemoteContainer = findViewById(R.id.remote_video_view_container);
 
         whiteboardView = findViewById(R.id.white);
         DWebView.setWebContentsDebuggingEnabled(true);
@@ -131,7 +228,103 @@ public class RoomActivity extends AppCompatActivity {
         }
     }
 
-    //region room
+    // 初始化 RtcEngine 对象
+    private void initializeEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.rtc_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e("TAG", Log.getStackTraceString(e));
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    private void setupVideoConfig() {
+        // In simple use cases, we only need to enable video capturing
+        // and rendering once at the initialization step.
+        // Note: audio recording and playing is enabled by default.
+        mRtcEngine.enableVideo();
+        // 详细设置查看 rtc 文档
+        mRtcEngine.setVideoEncoderConfiguration(new VideoEncoderConfiguration(
+                VideoEncoderConfiguration.VD_640x360,
+                VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15,
+                VideoEncoderConfiguration.STANDARD_BITRATE,
+                VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT));
+    }
+
+    private boolean checkSelfPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    private void setupLocalVideo() {
+        mLocalView = RtcEngine.CreateRendererView(getBaseContext());
+        mLocalView.setZOrderMediaOverlay(true);
+        mLocalContainer.addView(mLocalView);
+        mRtcEngine.setupLocalVideo(new VideoCanvas(mLocalView, VideoCanvas.RENDER_MODE_HIDDEN, 0));
+    }
+
+    public void onCallClicked(View view) {
+        if (mCallEnd) {
+            startCall();
+            mCallEnd = false;
+            mCallBtn.setImageResource(R.drawable.btn_endcall);
+        } else {
+            endCall();
+            mCallEnd = true;
+            mCallBtn.setImageResource(R.drawable.btn_startcall);
+        }
+    }
+
+    private void startCall() {
+        setupLocalVideo();
+        joinChannel();
+    }
+
+    private void endCall() {
+        removeLocalVideo();
+        removeRemoteVideo();
+        leaveChannel();
+    }
+
+    private void removeLocalVideo() {
+        if (mLocalView != null) {
+            mLocalContainer.removeView(mLocalView);
+        }
+        mLocalView = null;
+    }
+
+    private void removeRemoteVideo() {
+        if (mRemoteView != null) {
+            mRemoteContainer.removeView(mRemoteView);
+        }
+        mRemoteView = null;
+    }
+
+    private void leaveChannel() {
+        mRtcEngine.leaveChannel();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        leaveChannel();
+        RtcEngine.destroy();
+    }
+
+    /**
+     * 加入 rtc 频道
+     */
+    private void joinChannel() {
+        // 这里没有使用 token 加入频道，推荐使用 token 保证应用安全，详细设置参考 rtc 文档
+        mRtcEngine.joinChannel(null, "demoChannel1", "Extra Optional Data", 0);
+    }
+
+
+        //region room
     private void createRoom() {
         demoAPI.getNewRoom(new DemoAPI.Result() {
             @Override
@@ -178,43 +371,38 @@ public class RoomActivity extends AppCompatActivity {
         sdkConfiguration.setFonts(map);
 
         //图片替换 API，需要在 whiteSDKConfig 中先行调用 setHasUrlInterrupterAPI，进行设置，否则不会被回调。
-        WhiteSdk whiteSdk = new WhiteSdk(whiteboardView, RoomActivity.this, sdkConfiguration,
-                new UrlInterrupter() {
+        whiteSdk = new WhiteSdk(whiteboardView, this, sdkConfiguration,
+                new CommonCallbacks() {
                     @Override
                     public String urlInterrupter(String sourceUrl) {
                         return sourceUrl;
                     }
-                });
+
+                    @Override
+                    public void sdkSetupFail(SDKError error) {
+                        Log.e("ROOM_ERROR", error.toString());
+                    }
+
+                    @Override
+                    public void throwError(Object args) {
+
+                    }
+
+                    @Override
+                    public void onPPTMediaPlay() {
+                        logAction();
+                    }
+
+                    @Override
+                    public void onPPTMediaPause() {
+                        logAction();
+                    }
+                    // 如果用户需要用到 rtc 混音功能来解决回声和声音抑制问题，那么必须要在 whiteSDK 之前初始化 rtcEngine
+                    // AudioMixerBridgeImpl 在传入 sdk 后，ppt 内的音视频就全部使用 rtc 混音的方式播放
+                }, new AudioMixerBridgeImpl(mRtcEngine));
 
         /** 设置自定义全局状态，在后续回调中 GlobalState 直接进行类型转换即可 */
         WhiteDisplayerState.setCustomGlobalStateClass(MyGlobalState.class);
-
-        whiteSdk.setCommonCallbacks(new CommonCallbacks() {
-            @Override
-            public String urlInterrupter(String sourceUrl) {
-                return sourceUrl;
-            }
-
-            @Override
-            public void sdkSetupFail(SDKError error) {
-                Log.e("ROOM_ERROR", error.toString());
-            }
-
-            @Override
-            public void throwError(Object args) {
-
-            }
-
-            @Override
-            public void onPPTMediaPlay() {
-                logAction();
-            }
-
-            @Override
-            public void onPPTMediaPause() {
-                logAction();
-            }
-        });
 
         //如需支持用户头像，请在设置 WhiteSdkConfiguration 后，再调用 setUserPayload 方法，传入符合用户信息
         RoomParams roomParams = new RoomParams(uuid, roomToken);
